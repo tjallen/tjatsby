682b3ff9d41b572e03ea31cfc9fd8d149e6cec30
Thom Allen <thomwork@gmail.com>
2018-01-22 17:31:18 +0000

    Add a query

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'thomjamesallen.com',
+    tagline: '~ ~ ~ what a developer ~ ~ ~'
   },
   plugins: ['gatsby-plugin-react-helmet'],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,23 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>{data.site.siteMetadata.title}</h1>
+    <p>{data.site.siteMetadata.tagline}</p>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
-)
+);
+
+export const query = graphql`
+  query IndexQuery {
+    site {
+      siteMetadata {
+        title
+        tagline
+      }
+    }
+  }
+`
 
 export default IndexPage
