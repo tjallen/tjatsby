1e36d7d06cda21939a70e48a74a3f4b2c39db287
Thom Allen <thomwork@gmail.com>
2018-01-26 08:50:22 +0000

    Create slugs as node fields with gatsby-node onCreateNode to use later

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,1 +1,13 @@
-// node.
+const { createFilePath } = require('gatsby-source-filesystem');
+
+exports.onCreateNode = ({ node, getNode, boundActionCreators }) => {
+  const { createNodeField } = boundActionCreators;
+  if (node.internal.type === 'MarkdownRemark') {
+    const slugValue = createFilePath({ node, getNode, basePath: 'pages'});
+    createNodeField({
+      node,
+      name: 'slug',
+      value: slugValue,
+    });
+  };
+};
